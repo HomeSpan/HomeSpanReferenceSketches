2c0402bedf1ade45b3f095857636fea39c671ea5
Gregg <homespan@icloud.com>
2023-01-15 11:58:32 -0600

    Update Irrigation.ino

--- Irrigation/Irrigation.ino
+++ Irrigation/Irrigation.ino
@@ -34,23 +34,8 @@
  
 #include "HomeSpan.h"
 
-struct Sprinkler *sprinklerSystem;      // create global pointer to an instance of Sprinkler so we can reference it inside SpanUserCommand later on
-
-////////////////////////////////////////////////////////////////////////
-
-struct Sprinkler : Service::IrrigationSystem {
-
-  SpanCharacteristic *active=new Characteristic::Active(0);             // HomeKit requires this Characteristic, but it has no effect in Home App
-  SpanCharacteristic *programMode=new Characteristic::ProgramMode(0);   // HomeKit requires this Characteristic, but it is mostly for information purposeses only in the Home App
-
-  // NOTE: According to HAP-R2, the In Use Characteristic is also required for the Irrigation System Service.
-  // However, adding this Characteristic seems to break the Home App.  Seems that the HAP-R2 spec is out of date
-  // and that the Home App determines whether the Irrigation System is In Use simply by noting that one or more
-  // linked Valves are In Use.
-  //
-  // Recommendation: Do NOT instatiate Characteristic::InUse for the Irrigation Service
-
-};
+#define  HEAD_DURATION    20       // default duration, in seconds, for each Head to stay open (can be configured for each Head in Home App)
+#define  HEAD_SPEED       5000      // the time, in milliseconds, it takes for valve to open/close and water to fully flow or stop flowing
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -59,12 +44,12 @@ struct Head : Service::Valve {
   SpanCharacteristic *active=new Characteristic::Active(0);
   SpanCharacteristic *inUse=new Characteristic::InUse(0);
   SpanCharacteristic *enabled = new Characteristic::IsConfigured(1,true);
-  SpanCharacteristic *setDuration = new Characteristic::SetDuration(300);
+  SpanCharacteristic *setDuration = new Characteristic::SetDuration(HEAD_DURATION);
   SpanCharacteristic *remainingDuration = new Characteristic::RemainingDuration(0);
   SpanCharacteristic *name;
 
   Head(const char *headName) : Service::Valve() {
-    new Characteristic::ValveType(1);
+    new Characteristic::ValveType(1);                           // Ser Valve Type = Irrigation
     name=new Characteristic::ConfiguredName(headName,true);     // This Characteristic was introduced for TV Services, but works well here
     enabled->addPerms(PW);                                      // Adding "PW" to the IsConfigured Characteristic allows for enabling/disabling valves
   }
@@ -112,15 +97,105 @@ struct Head : Service::Valve {
       }
     }
 
-    // Below we simulate valves that take 5 seconds to open/close so that In Use follows Active by 5 seconds
-    // The Home App will accurately reflect this intermediate state and show "Waiting..." when a value of initially
-    // activated
+    // Below we simulate valves that take some fixed time to open/close so that changes in InUse lags changes in Active.
+    // The Home App will accurately reflect this intermediate state by displaying "Waiting..." when a value is initially opened.
     
-    if(active->timeVal()>5000 && active->getVal()!=inUse->getVal()){
+    if(active->timeVal()>HEAD_SPEED && active->getVal()!=inUse->getVal()){
       inUse->setVal(active->getVal());
       Serial.printf("%s value is %s\n",name->getString(),inUse->getVal()?"OPEN":"CLOSED");
     }
   }
+
+};
+
+////////////////////////////////////////////////////////////////////////
+
+struct Sprinkler : Service::IrrigationSystem {
+
+  // In this configuration we will LINK one or more Heads (i.e. Valve Services) to the Irrigation Service, INSTEAD of
+  // having the Irrigation Service be a standalone Service with unlinked Valves.  This means that the Home App will
+  // not display a separate "master" control for the Irrigation Service, and will instead automatically determine whether
+  // the system is Active according to whether one more values are Active.
+
+  // Also, because this configuration is based on LINKED Valves, DO NOT include Characteristic::InUse for the Irrigation Service
+
+  SpanCharacteristic *programMode=new Characteristic::ProgramMode(0);   // HomeKit requires this Characteristic, but it is only for display purposes in the Home App
+  SpanCharacteristic *active=new Characteristic::Active(0);             // though in this configuration the Home App will not display a "master" control, the Active Characteristic is still required
+  
+  vector<Head *> heads;                                                 // OPTIONAL: vector to store list of linked Heads that will be used for running a scheduled program
+  vector<Head *>::iterator currentHead;                                 // OPTIONAL: pointer to the current head in a scheduled program
+
+  Sprinkler(uint8_t numHeads) : Service::IrrigationSystem() {
+    
+    for(int i=1;i<=numHeads;i++){              // create Heads (Valves) and link each to the Sprinkler object
+      char name[16];
+      sprintf(name,"Head-%d",i);               // unique name for each Head that can be changed in the Home App
+      addLink(new Head(name));
+    }
+    
+    for(auto s : getLinks())                  // OPTIONAL: add each linked Head into storage vector for easy access below
+      heads.push_back((Head *)s);
+      
+    new SpanUserCommand('p', "- starts/stops scheduled program",startProgram,this);     // OPTIONAL: allows user to start a schedule program to sequentially turn on each enabled Head
+  }
+
+  static void startProgram(const char *buf, void *arg){     // OPTIONAL: start scheduled program
+    
+    Sprinkler *sprinkler=(Sprinkler *)arg;                      // recast the second arguments into a Sprinkler
+        
+    for(auto s : sprinkler->getLinks()) {                       // loop over all linked Services
+      Head *head = (Head *)s;                                   // recast linked Service to a Head
+      if(head->enabled->getVal() && head->active->getVal())     // if Head is both enabled and active
+        head->active->setVal(0);                                // turn off Heads
+    }
+
+    sprinkler->currentHead=sprinkler->heads.begin();                       // reset current head in scheduled program
+    sprinkler->active->setVal(0);                                          // set sprinkler Active to false
+    sprinkler->programMode->setVal(!sprinkler->programMode->getVal());     // toggle Program Mode
+
+    Serial.printf("Scheduled program: %s\n",sprinkler->programMode->getVal()?"STARTED":"STOPPED");
+  }
+
+  void loop(){                                              // OPTIONAL: only needed to support the running of scheduled programs
+     
+    if(!programMode->getVal())      // program mode not started
+      return;
+
+    if(currentHead==heads.end()){
+      Serial.printf("Scheduled program: COMPLETED\n");
+      programMode->setVal(0);
+      active->setVal(0);
+      return;
+    }
+
+    if(!(*currentHead)->enabled->getVal()){      // current Head is not enabled
+      currentHead++;                             // advance to next Head
+      return;
+    }
+
+    if((*currentHead)->active->getVal()){        // current Head is Active
+      if(!active->getVal()){                     // if sprinkler Active is still false (because user manually turned on Head)...
+        active->setVal(1);                       // ...set sprinkler Active to true
+        Serial.printf("Scheduled program: %s is ALREADY OPEN\n",(*currentHead)->name->getString());
+      }
+      return;
+      
+    } else if((*currentHead)->inUse->getVal()){  // current Head is not Active but still InUse
+      return;
+    }
+    
+    if(!active->getVal()){                       // current Head is not Active nor InUse and sprinkler Active is false...
+      active->setVal(1);                         // ...set sprinkler Active to true and turn on Head
+      (*currentHead)->active->setVal(1);
+      (*currentHead)->remainingDuration->setVal((*currentHead)->setDuration->getVal());
+      Serial.printf("Scheduled program: %s is OPENING\n",(*currentHead)->name->getString());
+      
+    } else if(!(*currentHead)->inUse->getVal()){  // wait for water to stop flowing in current Head before moving to next Head
+      active->setVal(0,false);
+      currentHead++;      
+    }
+    
+  } // loop()
 
 };
 
@@ -136,16 +211,11 @@ void setup() {
     new Service::AccessoryInformation();  
       new Characteristic::Identify();                           
                    
-    sprinklerSystem = new Sprinkler();
-    sprinklerSystem->addLink(new Head("Head 1"))
-                  ->addLink(new Head("Head 2"))
-                  ->addLink(new Head("Head 3"))
-                  ->addLink(new Head("Head 4"))
-                  ;
+    new Sprinkler(4);       // create Sprinkler with 4 Heads
 
-  // This allows user to toggle programMode on/off from Serial Monitor, though this sketch does not contain any actual scheduled programs:
+  // This allows user to toggle programMode on/off from Serial Monitor.  In practice this would be done using a button on the device or via a device-based display
   
-  new SpanUserCommand('p', "- starts/stops scheduled program",[](const char *buf){sprinklerSystem->programMode->setVal(!sprinklerSystem->programMode->getVal());});  
+//  new SpanUserCommand('p', "- starts/stops scheduled program",[](const char *buf){sprinkler->runProgram=!sprinkler->runProgram;});  
 
 } // end of setup()
 
